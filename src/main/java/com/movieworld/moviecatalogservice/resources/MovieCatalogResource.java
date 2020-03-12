19f03835fc02f92bdb92dd9c0ad9edee7c5fe3c1
riccardocecere <ric.cecere1@stud.uniroma3.it>
2020-03-12 12:39:48 +0100

    Microservice implemented as Eureka CLient; interaction whit microservices 'movie-info-service' and 'rating-info-service' is done through Eureka service discovery.

--- src/main/java/com/movieworld/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/movieworld/moviecatalogservice/resources/MovieCatalogResource.java
@@ -5,7 +5,6 @@ import com.movieworld.moviecatalogservice.models.Movie;
 import com.movieworld.moviecatalogservice.models.Rating;
 import com.movieworld.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,10 +30,10 @@ public class MovieCatalogResource {
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
 
-        UserRating ratings = restTemplete.getForObject("http://localhost:8083/ratingsdata/users/"+userId, UserRating.class);
+        UserRating ratings = restTemplete.getForObject("http://rating-data-service/ratingsdata/users/"+userId, UserRating.class);
 
         return ratings.getUserRating().stream().map(rating ->{
-                Movie movie = restTemplete.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+                Movie movie = restTemplete.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie.class);
 
                 return new CatalogItem(movie.getName(),"Desc", rating.getRating());
         })
